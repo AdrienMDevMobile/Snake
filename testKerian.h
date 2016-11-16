e97247ed2786156414b484d48149e497fca405f7
Kerian-NOEL <kerian1@wanadoo.fr>
2016-11-16 17:01:35 +0100

    INTERMEDIAIRE Kérian NOËL : ajout des classes tests Kérian

---
+++ testKerian.h
@@ -0,0 +1,21 @@
+#ifndef  TEST_KERIAN_H_INCLUDED
+#define  TEST_KERIAN_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include "output.h"
+
+#include "apple.h"
+#include "snakePart.h"
+#include "snakeList.h"
+#include "direction.h"
+#include "input_functions.h"
+
+void testCreationApple();
+void testInput();
+void createMapTest();
+
+#endif // TEST_KERIAN_H ENDS
+
